18446a395757d5d02c8c75acf3486fb04e23d3e2
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-10-25 00:41:21 -0400

    check for negative values and values above 20
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/commands/randomMotion.ts
+++ src/commands/randomMotion.ts
@@ -53,8 +53,16 @@ export const getRandomMotions = async (
     let amt = Number(message.content.split(" ")[1] ?? 5)
 
     if (isNaN(amt)) {
-        message.channel.send(`Could not parse ${message.content.split(" ")[1]} as a number. Learn to count.`)
+        message.channel.send(`Could not parse \`${message.content.split(" ")[1]}\` as a number. Learn to count.`)
 
+        return
+    }
+
+    if (amt > 20) {
+        message.channel.send(`Requested a total of ${amt} motions. That's too much power for me to handle. I'll be sending you 20 motions.`)
+        amt = 20
+    } else if (amt < 0) {
+        message.channel.send(`Requested a total of ${amt} motions. That's smaller than 0 (yes, I can count).\nBruh.`)
         return
     }
 
