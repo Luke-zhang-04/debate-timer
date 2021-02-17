707f6877633a74a5ff3687b01470aa8f0a021491
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-02-17 12:27:27 -0500

    update minification
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,40 @@
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
-import {terser} from "rollup-plugon-terser"
+import {terser} from "rollup-plugin-terser"
 
-export default {
+const banner = `#!/bin/node
+/**
+ * Discord Debate Timer
+ * @copyright 2020 Luke Zhang
+ * @author Luke Zhang luke-zhang-04.github.io/
+ * @version 1.4.1
+ * @license BSD-3-Clause
+ * @preserve
+ */
+
+`
+
+/**
+ * @type {import("rollup").RollupOptions}
+ */
+const config = {
     input: "lib/index.js",
     output: {
         file: "./bot.js",
-        format: "mjs",
+        format: "esm",
+        banner,
     },
     plugins: [
         commonjs(),
         resolve({
             resolveOnly: [/^\.{0,2}\//],
         }),
-        terser(),
+        terser({
+            format: {
+                comments: (_, {value}) => (/@preserve/ui).test(value),
+            }
+        }),
     ]
 }
+
+export default config
