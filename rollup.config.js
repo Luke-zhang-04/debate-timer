4a1e2ced507e51ac89ced5fb66e2c8c882d8aa98
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-02-17 12:23:32 -0500

    update build config
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
+import {terser} from "rollup-plugon-terser"
 
 export default {
     input: "lib/index.js",
     output: {
         file: "./bot.js",
-        format: "cjs",
+        format: "mjs",
     },
     plugins: [
         commonjs(),
         resolve({
             resolveOnly: [/^\.{0,2}\//],
         }),
+        terser(),
     ]
 }
