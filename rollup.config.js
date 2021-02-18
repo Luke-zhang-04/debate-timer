7ae19786405755cc503d02b3a39ee7cfb7a80481
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-02-17 21:13:54 -0500

    update version
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ const banner = `#!/bin/node
  * Discord Debate Timer
  * @copyright 2020 Luke Zhang
  * @author Luke Zhang luke-zhang-04.github.io/
- * @version 1.4.3
+ * @version 1.4.4
  * @license BSD-3-Clause
  * @preserve
  */
