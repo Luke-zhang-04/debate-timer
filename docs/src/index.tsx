02cf0f277c1a72811b1aee6b3b0bf87f76dcc875
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-03-10 21:58:47 -0500

    docs: add options for changing time
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -10,6 +10,12 @@ import DeStagnate from "destagnate"
 
 const minute = 60
 
+const parseNumber = (val: unknown): number | undefined => {
+    const number = Number(val ?? undefined)
+
+    return isNaN(number) ? undefined : number
+}
+
 /**
  * Turns seconds into human readable time
  * E.g `formatTime(90)` -> `"1:30"`
@@ -37,24 +43,53 @@ const formatTime = (secs: number): string => {
 interface State {
     time: number,
     paused: boolean,
+    protectedTime: number,
+    totalTime: number,
 }
 
 class Timer extends DeStagnate.Component<{}, State> {
+
+    private _timeForm = DeStagnate.createRef<HTMLInputElement>()
+
+    private _protectedForm = DeStagnate.createRef<HTMLInputElement>()
 
     private _intervalId = 0
 
     public constructor (parent: HTMLElement) {
         super(parent)
 
+        const time = parseNumber(localStorage.getItem("time"))
+        const protectedTime = parseNumber(localStorage.getItem("protectedTime"))
+
         this.state = {
             time: 0,
             paused: true,
+            protectedTime: protectedTime ?? 30,
+            totalTime: time ?? 5,
         }
     }
 
     public spacebar = (): void => {
         if (this.state.paused) {
             this.startTimer()
+
+            if (this.state.time === 0) {
+                const newTime = parseNumber(this._timeForm.current?.value)
+                const newProtected = parseNumber(this._protectedForm.current?.value)
+
+                if (
+                    newTime !== this.state.totalTime ||
+                    newProtected !== this.state.protectedTime
+                ) {
+                    localStorage.setItem("time", newTime?.toString() ?? "5")
+                    localStorage.setItem("protectedTime", newProtected?.toString() ?? "30")
+
+                    this.setState({
+                        protectedTime: newProtected,
+                        totalTime: newTime,
+                    })
+                }
+            }
         } else {
             clearInterval(this._intervalId)
         }
@@ -71,12 +106,15 @@ class Timer extends DeStagnate.Component<{}, State> {
     }
 
     public speechStatus = (): JSX.Element | undefined => {
+        const totalTime = this.state.totalTime * 60
+
         if (
-            this.state.time <= 30 ||
-            this.state.time >= 270 && this.state.time < 300
+            this.state.time <= this.state.protectedTime ||
+            this.state.time >= totalTime - this.state.protectedTime &&
+                this.state.time < totalTime
         ) {
             return <p class="status">Protected Time</p>
-        } else if (this.state.time >= 300) {
+        } else if (this.state.time >= totalTime) {
             return <p class="status">Times up!</p>
         }
 
@@ -93,20 +131,45 @@ class Timer extends DeStagnate.Component<{}, State> {
 
     public render = (): JSX.Element => {
         if (this.state.time === 0 && this.state.paused) {
-            return <>
-                <h1 class="header">Debate Timer</h1>,
+            return <div class="container">
+                <h1 class="header">Debate Timer</h1>
                 <p class="subheader">Space to pause/start. r to restart.</p>
-            </>
+
+                <form
+                    class="form"
+                    onSubmit={() => {
+                        console.log("SUBMIT")
+                    }}
+                >
+                    <label for="time">Time</label>
+                    <input
+                        type="number"
+                        name="time"
+                        value={this.state.totalTime}
+                        ref={this._timeForm}
+                    />
+                    <span>How long the timer should last in minutes</span>
+
+                    <label for="protected">Protected Time</label>
+                    <input
+                        type="number"
+                        name="protected"
+                        value={this.state.protectedTime}
+                        ref={this._protectedForm}
+                    />
+                    <span>Protected time at the beginning and end in seconds</span>
+                    <button type="submit">Start</button>
+                </form>
+            </div>
         }
 
         const status = this.speechStatus()
-        const content = <div class="container">
+
+        return <div class="container">
             <p class="time">{formatTime(this.state.time)}</p>
             <p class="status">{this.state.paused ? "Paused" : ""}</p>
             {status ? status : null}
         </div>
-
-        return content
     }
 
 }
