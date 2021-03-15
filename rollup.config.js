1cb6d100cb6937b95a5a36316d1906c7219e2492
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-03-15 10:26:22 -0400

    update copyright
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import typescript from "@rollup/plugin-typescript"
 const banner = `#!/bin/node
 /**
  * Discord Debate Timer
- * @copyright 2020 Luke Zhang
+ * @copyright 2020 - 2021 Luke Zhang
  * @author Luke Zhang luke-zhang-04.github.io/
  * @version 1.6.1
  * @license BSD-3-Clause
--- src/handleMessage.ts
+++ src/handleMessage.ts
@@ -1,6 +1,6 @@
 /**
  * Discord Debate Timer
- * @copyright 2020 Luke Zhang
+ * @copyright 2020 - 2021 Luke Zhang
  * @author Luke Zhang luke-zhang-04.github.io/
  * @version 1.6.1
  * @license BSD-3-Clause
