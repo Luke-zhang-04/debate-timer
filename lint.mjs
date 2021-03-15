f5c1b6b24a091fac91be9c60414d792b01970abe
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-03-15 18:40:25 -0400

    run lint
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- lint.mjs
+++ lint.mjs
@@ -2,39 +2,44 @@
 
 import chokidar from "chokidar"
 import {exec} from "child_process"
-import niceTry from "nice-try"
+
+const niceTry = (fn) => {
+    try {
+        return fn()
+    } catch (_) {}
+}
 
 const paths = {
-        eslint: (flags, dir) => (
-            `./node_modules/.bin/eslint_d "${dir}/{**/*.ts,*.ts}" ${flags}`
-        ),
-    },
+    eslint: (flags, dir) => (
+        `./node_modules/.bin/eslint_d "${dir}/{**/*.ts,*.ts}" ${flags}`
+    ),
+}
 
 
-    runLint = (path) => new Promise((resolve, reject) => {
-        try {
-            exec(path, (_, stdout, stderr) => {
-                if (stderr) {
-                    console.error(stderr)
-                    reject(stderr)
-                }
+const runLint = (path) => new Promise((resolve, reject) => {
+    try {
+        exec(path, (_, stdout, stderr) => {
+            if (stderr) {
+                console.error(stderr)
+                reject(stderr)
+            }
 
-                resolve(stdout)
-            })
-        } catch {
-            console.log("An error occured. The linter likely found a problem")
-        }
-    }),
-
-    lint = (dir = "*") => {
-        const flags = process.argv.includes("--fix") ||
-            process.argv.includes("--watch")
-                ? "--fix"
-                : "",
-            eslint = niceTry(() => runLint(paths.eslint(flags, dir)))
-
-        return Promise.all([eslint])
+            resolve(stdout)
+        })
+    } catch {
+        console.log("An error occured. The linter likely found a problem")
     }
+})
+
+const lint = (dir = "*") => {
+    const flags = process.argv.includes("--fix") ||
+            process.argv.includes("--watch")
+        ? "--fix"
+        : ""
+    const eslint = niceTry(() => runLint(paths.eslint(flags, dir)))
+
+    return Promise.all([eslint])
+}
 
 lint()
 
