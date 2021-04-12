6d25403fef9187a92dfe0c5f2091e46e5eedcd82
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-04-12 17:02:41 -0400

    version: bump to 1.9.0

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,7 +2,7 @@
  * Discord Debate Timer
  *
  * @license BSD-3-Clause
- * @version 1.8.0
+ * @version 1.9.0
  * @author Luke Zhang luke-zhang-04.github.io/
  * @copyright 2020 - 2021 Luke Zhang
  */
