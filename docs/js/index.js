7b4ff83a0e5c7aa77af80dfbb61a254eea902761
Luke <luke.zhang2004dev@gmail.com>
2021-04-13 10:20:26 -0400

    hosting: update script names (#26)

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,7 +2,7 @@
 * Discord Debate Timer
 * @copyright 2020 Luke Zhang
 * @author Luke Zhang luke-zhang-04.github.io/
-* @version 1.9.1
+* @version 1.9.2
 * @license BSD-3-Clause
 */
 !function(){"use strict";
