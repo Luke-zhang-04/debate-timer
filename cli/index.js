da4d460a87fed0a8b27087ea3ae904385c59dc55
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-04-13 10:12:42 -0400

    version: bump to 1.9.2

--- cli/index.js
+++ cli/index.js
@@ -3,7 +3,7 @@
  * Discord Debate Timer
  *
  * @license BSD-3-Clause
- * @version 1.9.1
+ * @version 1.9.2
  * @author Luke Zhang luke-zhang-04.github.io/
  * @copyright 2020 - 2021 Luke Zhang
  */
