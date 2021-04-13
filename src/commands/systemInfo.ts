56a6abe91512cac38a8837bca6de106b2d302d4b
Luke <luke.zhang2004dev@gmail.com>
2021-04-13 12:33:27 -0400

    fix(commands/systemInfo): use native modules to stop some bugs (#27)

--- src/commands/systemInfo.ts
+++ src/commands/systemInfo.ts
@@ -2,13 +2,18 @@
  * Discord Debate Timer
  *
  * @license BSD-3-Clause
- * @version 1.9.2
+ * @version 1.9.3
  * @author Luke Zhang luke-zhang-04.github.io/
  * @copyright 2020 - 2021 Luke Zhang
  */
 
+import os, {hostname} from "os"
 import childProcess from "child_process"
-import {hostname} from "os"
+
+const bytesPerGb = 1_073_741_824
+const memPrecision = 1_000_000
+
+let systemInfo: string | undefined
 
 /**
  * Run a shell command
@@ -20,7 +25,7 @@ const runCommand = (cmd: string): Promise<string> =>
         childProcess.exec(cmd, (err, stdout, stderr) => {
             if (stderr !== "" && !stderr.includes("WARNING")) {
                 // Ignore warnings
-                reject(stderr)
+                resolve(stderr)
             } else if (err !== null) {
                 reject(err)
             }
@@ -29,23 +34,30 @@ const runCommand = (cmd: string): Promise<string> =>
         })
     })
 
+const getSystemInfo = async (): Promise<string> => {
+    // Hardware info
+    const version = await runCommand("cat /proc/version").catch((err) =>
+        err instanceof Error ? `${err.name}: ${err.message}` : String(err),
+    )
+
+    // Platform name
+    const uname = await runCommand("uname -a").catch((err) =>
+        err instanceof Error ? `${err.name}: ${err.message}` : String(err),
+    )
+
+    // CPU info
+    const cpu = os.cpus()[0]?.model ?? "CPU Info unavailable"
+
+    // Memory (RAM)
+    const totalMem = Math.round((os.totalmem() * memPrecision) / bytesPerGb) / memPrecision
+    const freeMem = Math.round((os.freemem() * memPrecision) / bytesPerGb) / memPrecision
+
+    return `debate-timer-bot@${hostname()}\n\n${version}\nCPU: ${cpu}\nMemory: ${
+        totalMem - freeMem
+    }GB / ${totalMem}GB\n\n${uname}`
+}
+
 /**
  * Gets system details including hostname, CPU info, RAM, an uname
  */
-export default async (): Promise<string> => {
-    // Hardware info
-    const version = await runCommand("cat /proc/version")
-
-    // Platform name
-    const uname = await runCommand("uname -a")
-
-    // CPU info
-    const cpu = await runCommand('lscpu | grep -E "name" | tr -s " "')
-
-    // Memory (RAM)
-    const mem = await runCommand(
-        'cat /proc/meminfo | grep MemTotal | awk \'$3=="kB"{$2=$2/1024^2;$3="GB";} 1\'',
-    )
-
-    return `debate-timer-bot@${hostname()}\n\n${version}\n${cpu}${mem}\n${uname}`
-}
+export default async (): Promise<string> => (systemInfo ??= await getSystemInfo())
