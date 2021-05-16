767db7f22e1cd06a5513e1dbf1828f1fc4ca546e
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-05-15 22:57:54 -0400

    chore: update version

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * Discord Debate Timer
  *
  * @license BSD-3-Clause
- * @version 1.9.3
+ * @version 1.10.0
  * @author Luke Zhang luke-zhang-04.github.io/
  * @copyright 2020 - 2021 Luke Zhang
  */
